07c5cc22627a0a4d5d0c79e644d8b1e5b9c5369e
ariannecope <arianncope@gmail.com>
2026-06-30 15:33:56 -0500

    leap-year complete

--- leap-year/leap-year.js
+++ leap-year/leap-year.js
@@ -4,4 +4,22 @@
 Make an app that determines if a given year is a leap year! 
 
 Read the instructions in the README.md file in this folder.
+/*  ---------------------------
+💎 Leap Year Challenge
+------------------------------
+Make an app that determines if a given year is a leap year! 
+
+Read the instructions in the README.md file in this folder.
 ------------------------------ */
+// import moment node module
+import moment from "moment";
+
+// Accepts a year as user input from the command line with `process.argv` 
+let year = Number(process.argv[2]);
+
+// Outputs a friendly message with the result
+if (moment([year]).isLeapYear() === true) {
+console.log(`yay, Phil gets a birthday next year in ${year}`)
+} else {
+console.log(`boo, Phil does not get a birthday next year in ${year}`)
+}
